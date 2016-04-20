181ac273c8ad960c4b82653cd6acaab2fc1758d4
Jeff Samson <D208@campus.parkland.edu>
2016-04-20 10:46:11 -0500

    init setup

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
 
-console.log("hello world");
+var path = require('path');
+
+
+
+
+app.set("view engine", 'ejs');
+app.set('views', path.join(__dirname,'views'));
+
+app.get('/', function(req,res, next){
+    res.send("hello world");
+});
+
+app.use('/api', require('./routes/api'));
