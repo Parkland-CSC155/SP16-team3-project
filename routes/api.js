60410e4090e8ac9b5d6ad9d0f7f5f7a370401d25
Jeff Samson <D208@campus.parkland.edu>
2016-05-04 11:09:58 -0500

    moved to correct place

--- routes/api.js
+++ routes/api.js
@@ -15,6 +15,30 @@ router.get('/search', function (req,res) {
 
 
 
+router.get("/search/:searchText", function(req, res, next){
+    var page = req.query.page || 1
+    var searchText = req.params.searchText;
+    
+    var skip = (page -1) *25;
+    
+    var sql = `
+    SELECT * 
+    FROM NutritionDB
+    WHERE shrt_desc LIKE '${searchText}'
+    LIMIT 25 OFFSET ${skip}
+    `;
+    
+    db.all(sql, function(err, records){
+        if(err){
+            next(err);
+        }
+        
+    })
+    
+    res.json()
+    
+
+
 router.get('/list', function(req, res) {
     res.send("hello World");
 });
